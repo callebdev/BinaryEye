67cc43ebbccc35f533e73f26705242f7d3d74c18
Markus Fisch <mf@markusfisch.de>
2020-02-17 22:42:08 +0100

    Remove forceCompat() for new build tools
    Calling RenderScript.forceCompat() on a build that was made with the
    new build tools (which are required to make RenderScript build again
    on macOS Catalina), results in a RSRuntimeException (Loading of ScriptC
    script failed).
    
    RenderScript.forceCompat() was introduced to fix RenderScript for
    Lineage 16.0. Maybe this is no longer required with those new build
    tools.

--- app/src/main/kotlin/de/markusfisch/android/binaryeye/app/BinaryEyeApp.kt
+++ app/src/main/kotlin/de/markusfisch/android/binaryeye/app/BinaryEyeApp.kt
@@ -1,7 +1,6 @@
 package de.markusfisch.android.binaryeye.app
 
 import android.app.Application
-import android.support.v8.renderscript.RenderScript
 import de.markusfisch.android.binaryeye.preference.Preferences
 import de.markusfisch.android.binaryeye.repository.DatabaseRepository
 
@@ -11,11 +10,6 @@ val prefs = Preferences()
 class BinaryEyeApp : Application() {
 	override fun onCreate() {
 		super.onCreate()
-
-		// required to make RenderScript work for Lineage 16.0
-		// possibly because of a system/device bug
-		RenderScript.forceCompat()
-
 		db.open(this)
 		prefs.init(this)
 	}
